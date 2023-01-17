d02b800ad3dd1879d03b1a3cf44786812d69d9ed
javierDMC <javierdelmoralcamacho@gmail.com>
2023-01-17 22:05:11 +0100

    Add services(Subscribe in HeroesComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,12 +9,20 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-    hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
